886a5c8dd366609ce66382cc2e56f29ef73978f5
Teffisk <brad.mcknight91@gmail.com>
2018-12-20 22:21:00 -0800

    adding request url

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var layouts = require('express-ejs-layouts');
 var parser = require('body-parser');
+var geocoder = require('simple-geocoder');
+var request = require('request');
 
 // Declare your app
 var app = express();
@@ -16,13 +18,23 @@ app.use(parser.urlencoded({ extended: false }));
 
 // Declare routes
 app.get('/', function(req, res){
-  res.render('home');
+	res.render('home');
 });
 
 app.post('/', function(req, res){
-  res.render('result');
+	request('https://api.darksky.net/forecast/'+process.env.DARKSKY_API_KEY+'/'+locations.x+','+locations.y, function(err, response, body) {
+		if(err) {console.log('error: '+err);
+	} else {
+		var results = JSON.parse(body);
+		geocoder.geocode(req.body.userquery, function(success, locations) {
+			if(success) {
+				console.log("Location: ", locations.x, locations.y);
+			}
+			res.render('result', {userquery: req.body.userquery, longitude: locations.x, latitude: locations.y});
+			})
+		}
+	})
 });
-
 // Listen on PORT 3000
 app.listen(3000, function(){
   console.log('I\'m listening to the smooth sounds of port 3000 in the morning. ☕');
